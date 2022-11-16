d69d41ef37bbfb7c1bb6b80e77ea8294fab85d7a
Sublime-Sweetie <faith.Wallace.2@gmail.com>
2022-11-15 22:11:03 -0800

    generates markdown with questions

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -5,7 +5,9 @@ function renderLicenseBadge(license) {}
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {}
-
+// https://www.apache.org/licenses/LICENSE-2.0
+// https://choosealicense.com/licenses/mpl-2.0/
+// https://choosealicense.com/licenses/mit/
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {}
@@ -13,8 +15,39 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ## User Story:
+  ${data.description}
 
+  ## Table of Contents :
+  ${data.tableOfContents}
+
+  ## Installation:
+  ${data.installation}.
+
+  ## Useage:
+  Heres how you can use this app: 
+  ${data.useage}.
+
+  ## Credits: 
+  ${data.credits}.
+
+  ## License: 
+  ${data.license}.
+
+  ## Badges: 
+  ${data.badges}.
+
+  ## Features:
+   ${data.features}.
+
+  ## Tests: 
+  ${data.tests}.
+
+  ## Contributions & Contacts: 
+  If you are interested in collaborating or contributing to this project, please contact Faith Wallace at:
+  ${data.ccontact}.
 `;
 }
 
 module.exports = generateMarkdown;
+
--- npm/index.js
+++ npm/index.js
@@ -1,63 +1,92 @@
-const inquirer = require('inquirer');
+// TODO: Include packages needed for this application
 const fs = require("fs");
+const inquirer = require('inquirer');
+const generateMarkdown = require("../Develop/utils/generateMarkdown");
 
-inquirer
-  .prompt([
+
+// TODO: Create an array of questions for user input
+const questions=[
+    {
+      type: 'input',
+      name:'title',
+      message: "Please enter the name of your project."
+    },
     {
     type: 'input',
-    name:'Description',
+    name:'description',
     message: "Please enter 2-3 sentances describing your project."
   },
 
-   { type: 'checkbox',
-    name:'Table of Contents',
-    message: "Please enter your sections for the table of contents.",
-    choices: ["HTML", "CSS", "JavaScript"]
+  { 
+    type: 'checkbox',
+    name:'tableOfContents',
+    message: 'Please select your sections you want to include in the table of contents.',
+    choices: ["Description", "Installation", "Useage", "Credits", "License", "Badges", "Features", "Tests", "How to contribute"]
   },
 {
-    type: 'list',
-    name:'contact',
-    message: 'What is your preferred method of communication?',
-    choices:["email", "phone", "text"]
+    type: 'checkbox',
+    name:'installation',
+    message: 'What does your client need to install to make your project work?',
+    choices:["Browser", "Mobile", "custom hardware"]
   
   },
   {
     type: 'input',
-    name:'name',
-    message: "What is your name?"
-  
-  },
-
-   { type: 'checkbox',
-    name:'language',
-    message: "What languages do you know?",
-    choices: ["HTML", "CSS", "JavaScript"]
-  },
-{
-    type: 'list',
-    name:'contact',
-    message: 'What is your preferred method of communication?',
-    choices:["email", "phone", "text"]
+    name:'useage',
+    message: 'How is your project intended to be used?'
   
   },
   {
     type: 'input',
-    name:'name',
-    message: "What is your name?"},
+    name:'credits',
+    message: 'Enter the names & emails of all collaborators.'
+  
+  },
 
    { type: 'checkbox',
-    name:'language',
-    message: "What languages do you know?",
-    choices: ["HTML", "CSS", "JavaScript"]
+    name:'license',
+    message: 'Which License did you use?',
+    choices: ["MIT", "Apache v2.0", "General Public License"]
+  },
+  {
+    type: 'checkbox',
+    name:'badges',
+    message: 'Select which badges you want to add',
+    choices:["License badge", "Downloads", "Forks", "Socials"]
+    // https://img.shields.io/badge/License-MIT-blueviolet
+    // https://img.shields.io/badge/License-Apache2.0-blueviolet
+    // https://img.shields.io/badge/License-General%20Public-blueviolet
+  },
+  {
+    type: 'checkbox',
+    name:'features',
+    message: 'What features does this project provide?',
+    choices:["mobile compadability","dynamic programming","ease of access" ]
+  },
+  {
+    type: 'input',
+    name:'tests',
+    message: 'Please inert the links to the tests. Or enter N/A.',
+  },
+  {
+    type: 'input',
+    name:'contact',
+    message: 'Enter your email address or Github username.'
   }
+  ]
 
-  ])
-  .then((data) => {
-const filename = `${data.name.toLowerCase().split(" ").join(" ")}.json`
- fs.writeFile(filename, JSON.stringify(data, null, "\t"), (err) =>
- err ? console.log(err) : console.log("Success!!")
- );
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+return fs.writeFileSync(fileName, data)
+  }
+// TODO: Create a function to initialize app
+function init() {
+  inquirer.prompt(questions).then(response =>{ 
+    writeToFile('Readme.md', generateMarkdown(response))
+    console.log('Successfully created README.md!')
+  })
+}
 
-});
 
-
+// Function call to initialize app
+init();
